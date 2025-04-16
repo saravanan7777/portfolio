bf1a2f413ad71237cb44b2a48e31b67ed030687a
Saravanan <saravanan2003cr7@gmail.com>
2025-04-17 02:00:20 +0530

    final commit

---
+++ components/RecentProjects.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { FaLocationArrow } from "react-icons/fa6";
+
+import { projects } from "@/data";
+import { PinContainer } from "./ui/Pin";
+
+const RecentProjects = () => {
+  return (
+    <section id="projects">
+    <div className="py-20">
+      <h1 className="heading">
+        A small selection of{" "}
+        <span className="text-purple">recent projects</span>
+      </h1>
+      <div className="flex flex-wrap lg:grid lg:grid-cols-3  items-center justify-center p-4 gap-16 mt-10">
+        {projects.map((item) => (
+          <div
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw] cursor-pointer"
+            key={item.id}
+            onClick={() => window.open(item.link, '_blank')}
+          >
+            <PinContainer
+              title="Click"
+              href=""
+            >
+              <div className="relative flex items-center  justify-center sm:w-96 w-[80vw] overflow-hidden h-[50vh] lg:h-[35vh] mb-10">
+                <div
+                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
+                  style={{ backgroundColor: "#13162D" }}
+                >
+                  <img src="/bg.png" alt="bgimg" />
+                </div>
+                <img
+                  src={item.img}
+                  alt="cover"
+                  className="z-10 absolute w-[470px] h-[300px] "
+                />
+              </div>
+
+              <h1 className="font-bold lg:text-xl md:text-xl text-base line-clamp-1">
+                {item.title}
+              </h1>
+
+              <p
+                className="lg:text-l lg:font-normal font-light text-sm line-clamp-2"
+                style={{
+                  color: "#BEC1DD",
+                  margin: "1vh 0",
+                }}
+              >
+                {item.des}
+              </p>
+
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {item.iconLists.map((icon, index) => (
+                    <div
+                      key={index}
+                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index + 2}px)`,
+                      }}
+                    >
+                      <img src={icon} alt="icon5" className="p-2" />
+                    </div>
+                  ))}
+                </div>
+
+                <div className="flex justify-center items-center">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
+                </div>
+              </div>
+            </PinContainer>
+          </div>
+        ))}
+      </div>
+    </div>
+    </section>
+  );
+};
+
+export default RecentProjects;
---
+++ data/index.ts
@@ -0,0 +1,214 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Research Work", link: "#research" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I'm passionate about web development and AI ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a UI library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "A light-themed SaaS landing page",
+    des: "A clean, modern SaaS landing page that blends minimalist design with elegant functionality ",
+    img: "/p1.png",
+    iconLists: ["/next.svg", "/tail.svg",  "/fm.svg"],
+    link: "https://design1-pi.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Dynamic UI Playground Landing page",
+    des: "A sleek, modern web interface featuring intuitive drag-and-drop elements — blending interactive design",
+    img: "/p2.png",
+    iconLists: ["/next.svg", "/tail.svg",  "/fm.svg"],
+    link: "https://design2-tau.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "AI SaaS Landing Page",
+    des: "A dark-themed modern SaaS landing page with sleek animation with elegant functionality.",
+    img: "/p3.png",
+    iconLists: ["/next.svg", "/tail.svg",  "/fm.svg"],
+    link: "https://design3-seven.vercel.app/",
+  },
+  
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Email Spam Detection Using Machine Learning and Vectorization Techniques",
+    desc: "Objective of designing an efficient system that can detect spam with minimal computational cost, making it suitable for real-world applications in mail servers",
+    className: "md:col-span-2",
+    thumbnail: "/ieee.png",
+    link:"https://ieeexplore.ieee.org/document/10724545",
+  },
+  {
+    id: 2,
+    title: "Real-Time Sign Language Recognition Using MediaPipe and SVM",
+    desc: "In this research,a lightweight sustem was introduced that combines MediaPipe hand tracking SVM algorithm classification to recognize sign language in real-time.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/ieee.png",
+    link:"https://ieeexplore.ieee.org/document/10434034",
+  },
+  {
+    id: 3,
+    title: "A Cost-Sensitive Graph Neural Network Approach for Vulnerability Detection",
+    desc: "This research addresses the issues of blockchain platforms. The primary objective of this research is to identify critical smart contract vulnerabilities",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/ieee.png",
+    link:"https://ieeexplore.ieee.org/document/10846442",
+  },
+  {
+    id: 4,
+    title: "Enhancing SMS Spam Detection with Shallow Learning and Vectorization",
+    desc: "With a focus on the SMS Spam Collection Dataset, this research addresses the crucial challenge of classifying spam SMS.",
+    className: "md:col-span-2",
+    thumbnail: "/ieee.png",
+    link:"https://ieeexplore.ieee.org/document/10435941",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
